a76ccd60e880bd25bc0bd74fa1970e986b5ff1cd
Юрий Некрасов <furynoobas@gmail.com>
2019-09-14 09:59:26 +0300

    Add project files.

---
+++ DUDe/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DUDe.Startup))]
+namespace DUDe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
